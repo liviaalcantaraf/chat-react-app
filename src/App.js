feab6b1800c4c8664edb3dd38c8516409761d8ca
liviaalcantaraf <liviaalcantaraf@gmail.com>
2022-03-30 13:04:30 +0100

    created Chat Engine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import React from "react";
 import { ChatEngine } from "react-chat-engine";
 
 import "./App.css";
+
+const App = () => {
+  return (
+    <ChatEngine
+      projectID="4a32e800-f4ff-4d95-8076-094bd0295ee1"
+      userName="liviaalcantara"
+      userSecret="123123"
+    />
+  );
+};
+
+export default App;
