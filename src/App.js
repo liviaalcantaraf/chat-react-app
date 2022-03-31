621d1eadc234e3b83e0b06d159f43db4d9602827
liviaalcantaraf <liviaalcantaraf@gmail.com>
2022-03-31 21:18:12 +0100

    login code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import { ChatEngine } from "react-chat-engine";
+
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
+const projectID = "913bb1ba-b939-4bea-913e-310698647d69";
+
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
-      projectID="4a32e800-f4ff-4d95-8076-094bd0295ee1"
-      userName="liviaalcantara"
-      userSecret="123123"
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
